5f4e349dca39b6bb05b4de83f378f987edd127af
thatpentesterguy <220929227+thatpentesterguy@users.noreply.github.com>
2025-09-17 23:54:02 +0530

    created a seperate function for seperate task, this code demonstarates modularity

--- chapter2/excercise2/main.go
+++ chapter2/excercise2/main.go
@@ -1,8 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"strings"
+	"time"
 )
+
+// grep function to search a word in a file
+
+func grepString(filename string, searchstr string) {
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+	if strings.Contains(string(content), searchstr) {
+		fmt.Printf("Found the word %s in file %s\n", searchstr, filename)
+	} else {
+		fmt.Printf("Did not find the word %s in file %s\n", searchstr, filename)
+	}
+}
 
 func main() {
 
@@ -23,4 +40,22 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	searchstr := os.Args[1] // word to be searched
+	if searchstr == "" {
+		fmt.Println("Please provide a word to be searched as go run main.go <word> <file1> <file2> <file3>")
+		return
+	}
+	filenames := os.Args[2:] // list of files to be searched
+	if len(filenames) == 0 {
+		fmt.Println("Please provide textfile names as go run main.go <word> <file1> <file2> <file3>")
+		return
+	}
+
+	for _, filename := range filenames {
+		go grepString(filename, searchstr)
+	}
+
+	time.Sleep(2 * time.Second)
+
 }
