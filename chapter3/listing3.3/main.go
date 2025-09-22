23fa6af2c6d311b5652c3051b5cd040f21ef4405
thatpentesterguy <220929227+thatpentesterguy@users.noreply.github.com>
2025-09-23 01:14:16 +0530

    this version is with race condition

---
+++ chapter3/listing3.3/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"net/http"
+	"io"
+	"strings"
+	"fmt"
+	"time"
+)
+
+const allLetters = "abcdefghijklmnopqrstuvwxyz"
+
+func countLetters(url string, frequency []int) {
+
+	resp, _ := http.Get(url)
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		panic("Server returning error status code: " + resp.Status)
+	}
+
+	body, _ := io.ReadAll(resp.Body) // []byte
+
+	for _, b := range body {
+
+		c := strings.ToLower(string(b))
+
+		cIndex := strings.Index(allLetters, c)
+
+		if cIndex >= 0 {
+			frequency[cIndex] += 1
+		}
+
+
+	}
+	fmt.Println("Completed:", url)
+
+
+}
+
+func main() {
+
+	var frequency = make([]int, 26) // can be len(allLetters)
+
+	for i := 1000 ; i <= 1030; i++ {
+
+		url := fmt.Sprintf("https://rfc-editor.org/rfc/rfc%d.txt", i)
+		go countLetters(url, frequency)
+	}
+	time.Sleep(10 * time.Second) // wait for goroutines to finish
+	for i, v := range allLetters {
+		fmt.Printf("%c-%d ", v, frequency[i])
+	}
+
+}
