8bea3d17887061706bd0da72c1b9c15603815e9a
thatpentesterguy <220929227+thatpentesterguy@users.noreply.github.com>
2025-09-23 23:54:36 +0530

    heisenbugs

---
+++ chapter3/listing3.4/stingyspendy.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func stingy(money *int) {
+
+	for i :=0; i < 1000000; i++{
+		*money += 10
+	}
+	fmt.Println("Stingy Done")
+}
+
+func spendy(money *int) {
+	for i :=0; i < 1000000; i++{
+		*money -= 10
+	}
+	fmt.Println("Spendy Done")
+}
+
+func main() {
+
+	money := 100 
+	
+	go stingy(&money) // address of money 
+
+	go spendy(&money) // address of money
+	
+	time.Sleep(2 * time.Second)
+	fmt.Println("Money in Bank Account:", money)
+}
+
