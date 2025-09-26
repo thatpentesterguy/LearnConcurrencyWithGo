d73c10ec2e791545364ede44e5ea3865d4babcf2
thatpentesterguy <220929227+thatpentesterguy@users.noreply.github.com>
2025-09-26 23:09:20 +0530

    race condition continued

---
+++ chapter3/excercise3.2/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func addNextNumber(nextNum *[101]int) {
+	i := 0
+	for nextNum[i] != 0 { // Find the next available index
+		i++
+	}
+	nextNum[i] = nextNum[i-1] + 1
+}
+
+func main() {
+	nextNum := [101]int{1} // Initialize the first number to 1 rest
+
+	for i := 0; i < 100; i++ { // calling 100 times to fill the array
+		go addNextNumber(&nextNum)
+	}
+
+	for nextNum[100] == 0 {
+		println("Waiting for computation to finish...")
+		time.Sleep(1 * time.Millisecond)
+	}
+	fmt.Println(nextNum)
+}
