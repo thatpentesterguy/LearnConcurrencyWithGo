2242d29dd37c8f376be193118b38a7d20557dbc8
thatpentesterguy <220929227+thatpentesterguy@users.noreply.github.com>
2025-11-17 11:35:37 +0530

    exploring mutexes

---
+++ chapter4/listing4.1/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func stingy(money *int, mutex *sync.Mutex) {
+
+	for i := 0; i < 100000; i++ {
+
+		mutex.Lock()
+		*money += 10
+		mutex.Unlock()
+
+	}
+	fmt.Println("Stingy Done")
+}
+
+func spendy(money *int, mutex *sync.Mutex) {
+
+	for i := 0; i < 100000; i++ {
+
+		mutex.Lock()
+		*money -= 10
+		mutex.Unlock()
+
+	}
+	fmt.Println("Spendy Done")
+}
+
+func main() {
+
+	money := 100
+
+	mutex := sync.Mutex{} // Create a mutex
+
+	go stingy(&money, &mutex)
+	go spendy(&money, &mutex)
+
+	time.Sleep(2 * time.Second)
+
+	mutex.Lock()
+	fmt.Println("Money in bank account:", money) // read money safely
+	mutex.Unlock()
+}
